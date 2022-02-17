3b6de13057e0d5ea6669ea42194385307901d599
Bibi Ferreira <bibi.espeiorin@gmail.com>
2022-02-17 20:48:11 +0100

    watch list

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,5 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :restrict_with_exception
+  has_many :bookmarks
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
-  # validates :poster_url
-  # validates :rating
 end
